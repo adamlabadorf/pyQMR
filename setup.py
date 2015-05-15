ba2fa752a8bc64200d9a3dbb4c40a8ce72645c52
Adam Labadorf <alabadorf@gmail.com>
2015-05-15 15:03:02 -0400

    removed extraneous extras_require

--- setup.py
+++ setup.py
@@ -84,10 +84,10 @@ scripts necessary, short of specifying any environment-specific qsub options
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
+    #extras_require={
+    #    'dev': ['check-manifest'],
+    #    'test': ['coverage'],
+    #},
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
